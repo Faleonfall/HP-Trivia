d17f4bc0e69df976f2635fcbab1ac99e643cac83
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2024-11-30 21:22:45 +0100

    Initial Commit

---
+++ HP Trivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HP Trivia
+//
+//  Created by Volodymyr Kryvytskyi on 30.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Volodymyr Kryvytskyi on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
