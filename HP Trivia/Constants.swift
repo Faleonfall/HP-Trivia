d7fe0aa149b45ef63737538c4f4f42e06f6e0c6f
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2024-12-01 20:48:14 +0100

    Added elements in ContentView

---
+++ HP Trivia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HP Trivia
+//
+//  Created by Volodymyr Kryvytskyi on 01.12.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
