9d7c45d82a3b0193accad16983b42663e47baaa7
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2024-12-01 22:56:05 +0100

    Added Instructions screen

--- HP Trivia/Constants.swift
+++ HP Trivia/Constants.swift
@@ -5,8 +5,28 @@
 //  Created by Volodymyr Kryvytskyi on 01.12.2024.
 //
 
-import Foundation
+import SwiftUI
 
 enum Constants {
     static let hpFont = "PartyLetPlain"
+}
+
+struct InfoBackgroundImage: View {
+    var body: some View {
+        Image("parchment")
+            .resizable()
+            .ignoresSafeArea()
+            .background(.brown)
+    }
+}
+
+extension Button {
+    func doneButton() -> some View {
+        self
+            .font(.largeTitle)
+            .padding()
+            .buttonStyle(.borderedProminent)
+            .tint(.brown)
+            .foregroundStyle(.white)
+    }
 }
