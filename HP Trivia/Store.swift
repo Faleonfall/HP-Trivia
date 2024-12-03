760cc76843d5b7160b47f7de197bcac91a5aef13
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2024-12-03 13:35:18 +0100

    Added purchase functions

--- HP Trivia/Store.swift
+++ HP Trivia/Store.swift
@@ -18,14 +18,71 @@ enum BookStatus {
 class Store: ObservableObject {
     @Published var books: [BookStatus] = [.active, .active, .inactive, .locked, .locked, .locked, .locked]
     @Published var products: [Product] = []
+    @Published var purchaseIDs = Set<String>()
     
     private var productIds = ["hp4", "hp5", "hp6", "hp7"]
+    private var updates: Task<Void, Never>? = nil
+    
+    init() {
+        updates = watchForUpdates()
+    }
     
     func loadProducts() async {
         do {
             products = try await Product.products(for: productIds)
         } catch {
             print(error)
+        }
+    }
+    
+    func purchase(_ product: Product) async {
+        do {
+            let result = try await product.purchase()
+            
+            switch result {
+                
+            case .success(let verificationResult):
+                switch verificationResult {
+                case .unverified(let signedType, let verificationError):
+                    print("Error on \(signedType): \(verificationError)")
+                case .verified(let signedType):
+                    purchaseIDs.insert(signedType.productID)
+                }
+            case .userCancelled:
+                break
+            case .pending:
+                break
+            @unknown default:
+                break
+            }
+        } catch {
+            print("Purchase wasn't successful. \(error)")
+        }
+    }
+    
+    private func checkPurchased() async {
+        for product in products {
+            guard let state = await product.currentEntitlement else { return }
+            
+            switch state {
+            case .unverified(let signedType, let verificationError):
+                print("Error on \(signedType): \(verificationError)")
+                
+            case .verified(let signedType):
+                if signedType.revocationDate == nil {
+                    purchaseIDs.insert(signedType.productID)
+                } else {
+                    purchaseIDs.remove(signedType.productID)
+                }
+            }
+        }
+    }
+    
+    private func watchForUpdates() -> Task<Void, Never> {
+        Task(priority: .background) {
+            for await _ in Transaction.updates {
+                await checkPurchased()
+            }
         }
     }
 }
